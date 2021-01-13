dc8001304cbcef6d09ba0ed35f26008d366b6dca
bethpritchard <60654572+bethpritchard@users.noreply.github.com>
2021-01-13 22:07:12 +0000

    coffee machine OOP

--- Day16/coffee_machine_oop.py
+++ Day16/coffee_machine_oop.py
@@ -1,6 +1,11 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+"""
+Coffee machine main code written by me, classes by Angela Yu at London App Brewery
+"""
+
+
 
 def coffee_machine():
     """
