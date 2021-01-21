03ced5342acf1752357f16767071ccbd58f1725e
bethpritchard <60654572+bethpritchard@users.noreply.github.com>
2021-01-21 16:43:18 +0000

    snake

--- Day20/snake.py
+++ Day20/snake.py
@@ -14,26 +14,33 @@ class Snake(Turtle):
         super().__init__()
         self.segments = []
         self.create_snake()
-        self.head =self.segments[0]
-        self.tail = self.segments[-1]
+        self.head = self.segments[0]
+        # self.tail = self.segments[-1]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
             self.add_segment(position)
 
-
-    def add_segment(self,position):
+    def add_segment(self, position):
         new_segment = Turtle(shape="square")
         new_segment.color("white")
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
 
+    def reset_segments(self):
+        for segment in self.segments:
+            segment.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+        # self.tail = self.segments[-1]
+
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
     def move(self):
-        for seg_num in range(len(self.segments) - 1, 0, -1): #moves each turtle to positon of last one
+        for seg_num in range(len(self.segments) - 1, 0, -1):  # moves each turtle to positon of last one
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
--- Day20/snake_game_main.py
+++ Day20/snake_game_main.py
@@ -24,30 +24,30 @@ screen.onkey(snake.move_down, "Down")
 screen.onkey(snake.move_left, "Left")
 screen.onkey(snake.move_right, "Right")
 
-def run_game():
-    game_is_on = True
-    while game_is_on:
-        screen.update()
-        time.sleep(0.1)
-        snake.move()
 
-        # Detect collision with food
-        if snake.head.distance(food) < 17:
-            food.move_food()
-            snake.extend()
-            scoreboard.increase_score()
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
 
-        # Detect collision with wall
-        if abs(snake.head.xcor()) > (WIDTH / 2) - 10 or abs(snake.head.ycor()) > (HEIGHT / 2) - 10:
-            game_is_on = False
-            scoreboard.game_over()
+    # Detect collision with food
+    if snake.head.distance(food) < 17:
+        food.move_food()
+        snake.extend()
+        scoreboard.increase_score()
 
-        # Detect collision with tail
-        for segment in snake.segments[1:]:
-            if snake.head.distance(segment) < 0:
-                game_is_on = False
-                scoreboard.game_over()
+    # Detect collision with wall
+    if abs(snake.head.xcor()) > (WIDTH / 2) - 10 or abs(snake.head.ycor()) > (HEIGHT / 2) - 10:
+        snake.reset_segments()
+        scoreboard.reset_score()
 
-run_game()
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 0:
+            snake.reset_segments()
+            scoreboard.reset_score()
+
+
 
 screen.exitonclick()
