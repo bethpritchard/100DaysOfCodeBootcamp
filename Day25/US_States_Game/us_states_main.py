94eb13455a09cd9928e382a860a123abb092c5a4
bethpritchard <60654572+bethpritchard@users.noreply.github.com>
2021-01-23 16:36:18 +0000

    add add_state function

--- Day25/US_States_Game/us_states_main.py
+++ Day25/US_States_Game/us_states_main.py
@@ -2,7 +2,7 @@ import turtle
 import pandas as pd
 
 screen = turtle.Screen()
-screen.screensize(900,900)
+screen.setup(width = 1.0, height = 1.0)
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 
@@ -14,6 +14,13 @@ all_states = data.state.tolist()
 states_guessed = []
 
 
+def add_state(state, x, y):
+    t = turtle.Turtle(visible = False)
+    t.penup()
+    t.speed("fastest")
+    t.goto(int(x), int(y))
+    t.write(state)
+
 
 while len(states_guessed) < 51:
     answer_state = screen.textinput(title = f"{len(states_guessed)}/50 States Correct", prompt="Enter a state").title()
@@ -21,27 +28,16 @@ while len(states_guessed) < 51:
         missing_states = []
         for state in all_states:
             if state not in states_guessed:
-                t = turtle.Turtle()
-                t.hideturtle()
-                t.penup()
-                t.speed("fastest")
                 state_data = data[data.state == state]
-                t.goto(int(state_data.x), int(state_data.y))
-                t.write(state)
-
+                add_state(state,state_data.x, state_data.y)
+                missing_states.append(state)
         new_data = pd.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
     if answer_state in all_states and answer_state not in states_guessed:
         states_guessed.append(answer_state)
-
-        t = turtle.Turtle()
-        t.hideturtle()
-        t.penup()
-
         state_data = data[data.state == answer_state]
-        t.goto(int(state_data.x),int(state_data.y))
-        t.write(answer_state)
+        add_state(answer_state, state_data.x, state_data.y)
 
 
 
