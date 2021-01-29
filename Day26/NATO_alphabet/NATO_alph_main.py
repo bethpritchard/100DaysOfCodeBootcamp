1f0eca64ea1fa00f224aee4c38cfd8a69be5ea04
bethpritchard <60654572+bethpritchard@users.noreply.github.com>
2021-01-29 12:43:25 +0000

    add error handling

--- Day26/NATO_alphabet/NATO_alph_main.py
+++ Day26/NATO_alphabet/NATO_alph_main.py
@@ -2,12 +2,17 @@ import pandas
 
 
 df = pandas.read_csv("nato_phonetic_alphabet.csv")
-alphabet = {row.letter:row.code for (index, row) in df.iterrows()}
+phonetic_dict = {row.letter:row.code for (index, row) in df.iterrows()}
 
+def convert():
+    word = input("Please input word to convert: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, alphabet only")
+        convert()
 
-user_input = str(input("Please input word to convert: ")).upper()
+    else:
+        print(output_list)
 
-user_list = [letter for letter in user_input]
-
-
-print([code for (letter, code) in alphabet.items() if letter in user_list])
+convert()
