fc8eb7dd9e05481ec3415ae52fd878d899702fbc
bethpritchard <60654572+bethpritchard@users.noreply.github.com>
2021-02-03 11:38:34 +0000

    kanye quote generator 🔥

---
+++ Day33/Kanye_quotes/kanye_quote_main.py
@@ -0,0 +1,35 @@
+from tkinter import *
+import requests
+
+URl = "https://api.kanye.rest"
+
+
+def get_quote():
+
+    response = requests.get("https://api.kanye.rest")
+    if response.status_code == 200:
+        quote_dict = response.json()
+        quote = quote_dict["quote"]
+        canvas.itemconfig(quote_text,text =quote)
+
+
+
+window = Tk()
+window.title("Kanye Says...")
+window.config(padx=50, pady=50)
+
+
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file="background.png")
+canvas.create_image(150, 207, image=background_img)
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 20, "bold"), fill="white")
+canvas.grid(row=0, column=0)
+get_quote()
+
+kanye_img = PhotoImage(file="kanye.png")
+kanye_button = Button(image=kanye_img, highlightthickness=0, borderwidth=0, command=get_quote)
+kanye_button.grid(row=1, column=0)
+
+
+
+window.mainloop()
