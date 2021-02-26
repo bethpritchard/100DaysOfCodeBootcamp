4b3f1dbb922a8f6662c2e3f26d101cc507210a79
Beth <bethpritchard97@gmail.com>
2021-02-26 23:26:36 +0000

    add create playlist functionality

--- Day46/spotify_time_travel.py
+++ Day46/spotify_time_travel.py
@@ -1,17 +1,18 @@
 from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
+from pprint import pprint
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os
-
+# ------------- SET UP -----------
 client_id = os.environ.get("CLIENT_ID")
 client_secret = os.environ.get("CLIENT_SECRET")
 
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
-        scope="playlist-modify-private",
+        scope="playlist-modify-private playlist-modify-public",
         redirect_uri="http://example.com",
         client_id=client_id,
         client_secret=client_secret,
@@ -22,17 +23,20 @@ sp = spotipy.Spotify(
 user_id = sp.current_user()["id"]
 
 
+#---------------- get date ---------
+
+date_text = input("What date would you like to travel to? Input in format DD/MM/YYYY: ")
+original_date = date_text
+date_text = date_text.split('/')
+date = ""
+for elem in date_text[::-1]:
+    date+= elem
 
 
+year = date[:4]
 
-# date_text = input("What year would you like to travel to? Input in format DD/MM/YYYY: ").split("/")
-#
-# date = ""
-# for elem in date_text[::-1]:
-#     date+= elem
-# date = int(date)
-date = 19970901
 
+# -------------------- get songs -----------------
 url = f"https://www.officialcharts.com/charts/singles-chart/{date}/7501"
 
 response = requests.get(url)
@@ -40,7 +44,29 @@ charts_site = response.text
 soup=BeautifulSoup(charts_site,"html.parser")
 
 songs = soup.find_all(name="div", class_="title")
-song_names = [song.get_text().replace("\n", " ").strip() for song in songs]
+song_names = [song.get_text().replace("\n", " ").strip().title() for song in songs]
+
+# ----------------- get song uris ---------------------
+song_uris = []
+
+for song in song_names:
+    result = sp.search(f"track: {song} year: {year}", type = "track")
+
+    try:
+        uri = result["tracks"]['items'][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"Error: {song} does not exist on Spotify")
 
 
+# # ---------------- create playlist ------------------------
+playlist_name = f"Time Travel Back to {original_date}"
+playlist_description = f"Time travel with Python!"
+playlist = sp.user_playlist_create(user_id,
+                                   playlist_name,
+                                   public=True,
+                                   description=playlist_description)
 
+result = sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
+
+print("Playlist compiled!")
