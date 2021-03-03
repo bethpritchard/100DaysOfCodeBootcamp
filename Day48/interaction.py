a868cc52a57d968d72c6679b43460e9904360af7
Beth <bethpritchard97@gmail.com>
2021-03-03 10:17:41 +0000

    update

--- Day48/interaction.py
+++ Day48/interaction.py
@@ -1,13 +1,16 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 chrome_driver_path = r"C:\Users\Bethany Pritchard\Dev\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
-article_count = driver.find_element_by_css_selector("#articlecount a").text
+article_count = driver.find_element_by_css_selector("#articlecount a")
 
-print(article_count)
+all_portals = driver.find_element_by_link_text("All portals")
 
+search = driver.find_element_by_name("search")
 
-driver.close()
+search.send_keys("Python")
+search.send_keys(Keys.ENTER)
