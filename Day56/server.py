d8285d317388697d87c4a6e06549b878de74daa2
Beth <bethpritchard97@gmail.com>
2021-03-17 20:33:40 +0000

    update

--- Day56/server.py
+++ Day56/server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template("index.html")
+    return render_template("guess.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Day57/server.py
+++ Day57/server.py
@@ -18,7 +18,7 @@ def hello():
 def guess(name):
     year = datetime.date.today().year
 
-    params = {"name": name.title()}
+    params = {"name": name}
     age_response = requests.get(url="https://api.agify.io/", params=params)
     age_dict = age_response.json()
     age = age_dict["age"]
@@ -27,7 +27,15 @@ def guess(name):
     gender_dict = gender_response.json()
     gender = gender_dict["gender"]
 
-    return render_template("index.html", year=year, age=age, name=name, gender=gender)
+    return render_template("guess.html", year=year, age=age, name=name, gender=gender)
+
+@app.route("/blog/<num>")
+def get_blog(num):
+    blog_url = "https://api.npoint.io/5abcca6f4e39b4955965"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
