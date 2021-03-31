9c54746c47470aa384e5958e75fcee13dadc64a5
Beth <bethpritchard97@gmail.com>
2021-03-31 19:48:17 +0100

    Update main.py

--- Day59/main.py
+++ Day59/main.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
+
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    return f"<H1>Username: {username} Password: {password}</H1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
