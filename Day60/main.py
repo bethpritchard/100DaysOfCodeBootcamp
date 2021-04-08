d6408bd6d7cb9420787aa8d2cf62e2d6821fa880
Beth <bethpritchard97@gmail.com>
2021-04-08 20:56:53 +0100

    update

--- Day60/main.py
+++ Day60/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from flask_bootstrap import Bootstrap
+
 from wtforms.validators import DataRequired
 import os
 
@@ -11,7 +13,7 @@ class LoginForm(FlaskForm):
 
 
 app = Flask(__name__)
-
+Bootstrap(app)
 app.secret_key = os.environ.get("SECRETKEY")
 
 
@@ -31,6 +33,11 @@ def home():
 @app.route("/login", methods=('GET', 'POST'))
 def login():
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "1234":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
     return render_template("login.html", form=login_form)
 
 
