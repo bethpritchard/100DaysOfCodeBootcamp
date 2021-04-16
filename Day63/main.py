82fd1ed3916cfc91a2ae3be7c79769059c0e4c16
Beth <bethpritchard97@gmail.com>
2021-04-16 20:48:15 +0100

    Update main.py

--- Day63/main.py
+++ Day63/main.py
@@ -27,15 +27,7 @@ def home():
 
 @app.route("/add",methods=["GET", "POST"])
 def add():
-
     if request.method == "POST":
-        # new_book = {
-        #     "title": request.form["title"],
-        #     "author": request.form["author"],
-        #     "rating": request.form["rating"],
-        # }
-        # all_books.append(new_book)
-
         new_book = Book(title=request.form["title"],
                         author=request.form["author"],
                         rating=request.form["rating"])
@@ -44,6 +36,34 @@ def add():
 
         return redirect(url_for('home'))
     return render_template("add.html")
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        # UPDATE RECORD
+        book_id = request.form["id"]
+        book_to_update = Book.query.get(book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get('id')
+    book_selected = Book.query.get(book_id)
+    return render_template("edit_rating.html", book=book_selected)
+
+
+
+@app.route("/delete")
+def delete():
+
+    book_id = request.args.get('id')
+
+    # DELETE A RECORD BY ID
+    book_to_delete = Book.query.get(book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
